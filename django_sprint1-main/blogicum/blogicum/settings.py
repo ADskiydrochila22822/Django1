d872d1de16dd330dcfe578b1a61f33b05917962f
Saman2 <pggrrrr@gmail.com>
2026-01-14 09:33:46 +0500

    Update settings.py

--- django_sprint1-main/blogicum/blogicum/settings.py
+++ django_sprint1-main/blogicum/blogicum/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k#*g)rz#!od#ci%r-y+eh0s#1zvs6mp66%qe^sh7k^_q86!tn&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [http://127.0.0.1:8000/]
+ALLOWED_HOSTS = []
 
 
 # Application definition
